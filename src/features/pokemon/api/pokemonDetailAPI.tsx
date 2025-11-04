82022c094f27b2eb9ba4c916c8524dafeb6eccde
vietdan1613 <viet.dan.1613@gmail.com>
2025-11-04 14:14:33 +0700

    first commit

--- src/features/pokemon/api/pokemonDetailAPI.tsx
+++
@@ -1,7 +0,0 @@
-import type { PokemonDetail } from "../types/pokemonTypes";
-
-export const fetchPokemonDetail = async (name: string): Promise<PokemonDetail> => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    if (!res.ok) throw new Error("Failed to fetch Pokémon detail");
-    return res.json();
-};
